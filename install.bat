b53fdbb56f381b0c451c01bebc7083f7465118f8
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-03 16:37:38 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,11 @@
 @echo off
 echo downloading...
 set npath="%TEMP%\udev265SAP\"
-mkdir "%NPATH%"
+mkdir "%npath%"
 set "NPATH=%NPATH:\=/%"
 powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/home4anybrowser.html'; Invoke-WebRequest -Uri $uri -Method Get -OutFile \"%NPATH%/homepage.html\""
 powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png'; Invoke-WebRequest -Uri $uri -Method Get -OutFile \"%NPATH%/background.png\""
 powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/bg.png'; Invoke-WebRequest -Uri $uri -Method Get -OutFile \"%NPATH%/bg.png\""
 powershell -command "Start-Process -FilePath 'msedge' -ArgumentList 'file://%NPATH%/homepage.html'"
 timeout /t 1
-del %userprofie%\install.bat
-exit
+(goto) 2>nul & del "%~f0"
