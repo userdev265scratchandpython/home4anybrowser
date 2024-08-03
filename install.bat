4c9430d1b746cadba92ef528f09107d9db9950f7
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-03 16:30:34 +0200

    autodelete

--- install.bat
+++ install.bat
@@ -7,4 +7,6 @@ powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/
 powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png'; Invoke-WebRequest -Uri $uri -Method Get -OutFile \"%NPATH%/background.png\""
 powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/bg.png'; Invoke-WebRequest -Uri $uri -Method Get -OutFile \"%NPATH%/bg.png\""
 powershell -command "Start-Process -FilePath 'msedge' -ArgumentList 'file://%NPATH%/homepage.html'"
+timeout /t 1
+del %userprofie%\install.bat
 exit
