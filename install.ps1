57ea9507a67c495a35b2148b073801648be95694
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-03 14:35:30 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -5,9 +5,9 @@ if (!(Test-Path -Path $npath)) {
 }
 $npath = $npath -replace '\\', '/'
 $uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/home4anybrowser.html'
-Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/homepage.html" -Force
+Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/homepage.html"
 $uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png'
-Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/background.png" -Force
+Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/background.png"
 $uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/bg.png'
-Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/bg.png" -Force
+Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/bg.png"
 Start-Process -FilePath 'msedge' -ArgumentList "file://$npath/homepage.html"
