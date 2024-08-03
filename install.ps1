fce65a2ef35d295d2cde936747fda399228670a2
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-03 14:47:31 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
 [Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/install.bat'
 Invoke-WebRequest -Uri $uri -Method Get -OutFile "$env:USERPROFILE\install.bat"
 Start-Process -FilePath "cmd" -ArgumentList "/c", "$env:USERPROFILE\install.bat"
+exit
