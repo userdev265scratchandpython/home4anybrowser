ef4690c82cedb11b7c7401074a37a5c39f5a6206
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-03 14:34:30 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,8 @@
 $npath = "$env:APPDATA\temp"
 Remove-Item -Path $npath -Recurse -Force -ErrorAction SilentlyContinue
-New-Item -ItemType Directory -Path $npath -Force -ErrorAction SilentlyContinue
+if (!(Test-Path -Path $npath)) {
+    New-Item -ItemType Directory -Path $npath -Force
+}
 $npath = $npath -replace '\\', '/'
 $uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/home4anybrowser.html'
 Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/homepage.html" -Force
