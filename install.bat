215f1ce33849d3fb109603ea90fdb7344651800c
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-03 16:49:40 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -7,5 +7,3 @@ powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/
 powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png'; Invoke-WebRequest -Uri $uri -Method Get -OutFile \"%NPATH%/background.png\""
 powershell -command "[Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/bg.png'; Invoke-WebRequest -Uri $uri -Method Get -OutFile \"%NPATH%/bg.png\""
 powershell -command "Start-Process -FilePath 'msedge' -ArgumentList 'file://%NPATH%/homepage.html'"
-timeout /t 1
-(goto) 2>nul & del "%~f0"
