d354a34c72be1f397fb94429b1911ac8a67fcf53
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-03 14:32:00 +0200

    fixed issues

--- install.ps1
+++ install.ps1
@@ -1,10 +1,11 @@
 $npath = "$env:APPDATA\temp"
-New-Item -ItemType Directory -Path $npath
+Remove-Item -Path $npath -Recurse -Force -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Path $npath -Force
 $npath = $npath -replace '\\', '/'
 $uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/home4anybrowser.html'
-Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/homepage.html"
+Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/homepage.html" -Force
 $uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png'
-Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/background.png"
+Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/background.png" -Force
 $uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/bg.png'
-Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/bg.png"
-Start-Process -FilePath 'sedge' -ArgumentList "file://$npath/homepage.html"
+Invoke-WebRequest -Uri $uri -Method Get -OutFile "$npath/bg.png" -Force
+Start-Process -FilePath 'msedge' -ArgumentList "file://$npath/homepage.html"
