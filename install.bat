871ed3aed82687df776db1e96d9a878d0a4b08db
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-04 11:40:27 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,7 +1,8 @@
 @echo off
 echo downloading...
 set npath="%TEMP%\udev265SAP\"
-if not exist "%npath%" mkdir "%npath%"
+if exist "%npath%" rmdir /s /q "%npath%"
+mkdir "%npath%"
 
 powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/home4anybrowser.html' -Method Get -OutFile '%npath%homepage.html'"
 powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png' -Method Get -OutFile '%npath%background.png'"
