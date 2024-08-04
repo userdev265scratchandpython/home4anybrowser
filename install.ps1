6a1b4cd988c868cc15b91fc697e1658612edbe5d
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-04 10:00:38 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,27 +1,5 @@
-# Set the URL of the batch file
 [Uri]$uri = 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/install.bat'
-
-# Print a message to the console
-Write-Output "Downloading from: $uri"
-Start-Sleep -s 2
-
-try {
-    # Download the batch file
-    Invoke-WebRequest -Uri $uri -Method Get -OutFile "$env:TEMP\install.bat"
-} catch {
-    # Handle any errors that occur during the download
-    Write-Warning "Failed to download batch file: $($Error[0].Message)"
-    exit 1
-}
-
-try {
-    # Run the batch file
-    Start-Process -FilePath "cmd" -ArgumentList "/c", "$env:TEMP\install.bat" -Wait
-} catch {
-    # Handle any errors that occur while running the batch file
-    Write-Warning "Failed to run batch file: $($Error[0].Message)"
-    exit 1
-}
-
-# Exit the PowerShell script
-exit 0
+Write-Output "download from : $uri"
+Invoke-WebRequest -Uri $uri -Method Get -OutFile "$env:TEMP\install.bat"
+Start-Process -FilePath "cmd" -ArgumentList "/c", "$env:TEMP\install.bat"
+exit
