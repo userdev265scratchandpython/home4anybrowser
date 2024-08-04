1e2fb5e7507f73cfc7845f9bb7739d7169112cf1
userdev265scratchandpython <149082873+userdev265scratchandpython@users.noreply.github.com>
2024-08-04 11:46:53 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,8 +4,8 @@ set npath="C:\Users\%username%\AppData\Roaming\temp\udev265SAP\"
 if exist "%npath%" rmdir /s /q "%npath%"
 mkdir "%npath%"
 
-powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/home4anybrowser.html' -Method Get -OutFile '%npath%homepage.html'"
-powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png' -Method Get -OutFile '%npath%background.png'"
-powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/bg.png' -Method Get -OutFile '%npath%bg.png'"
+powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/home4anybrowser.html' -Method Get | Set-Content -LiteralPath '%npath%homepage.html'"
+powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/background.png' -Method Get | Set-Content -LiteralPath '%npath%background.png' -Encoding Byte"
+powershell -command "Invoke-WebRequest -Uri 'https://github.com/userdev265scratchandpython/home4anybrowser/raw/main/bg.png' -Method Get | Set-Content -LiteralPath '%npath%bg.png' -Encoding Byte"
 
 start "" "%npath%homepage.html"
